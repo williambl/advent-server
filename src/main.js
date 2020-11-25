4f706002ffdbcec3c8ccab6a3c7248da8db5f69b
williambl <william@williambl.com>
2020-11-25 11:02:00 +0000

    Add basic user score storing

--- src/main.js
+++ src/main.js
@@ -1,12 +1,35 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const users = []
+
+const userScoreMap = new Map()
+
+const checkUser = (user) => {
+    if (users.includes(user))
+        return true
+    users.push(user)
+    userScoreMap.set(user, [])
+    return false
+}
+
+app.use(cookieParser())
+
+app.get('/api/challengesCompleted', (req, res) => {
+    if (req.cookies == undefined || req.cookies['auth'] == undefined) {
+        res.status = 401
+        res.end()
+        return
+    }
+    checkUser(req.cookies['auth'])
+    res.status = 200
+    res.end(JSON.stringify(userScoreMap.get(req.cookies['auth'])))
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
 
