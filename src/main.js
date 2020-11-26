e4e399025e2db02560af8c2019937aee981d5d26
williambl <william@williambl.com>
2020-11-26 16:03:50 +0000

    Fix setting response status code wrongly

--- src/main.js
+++ src/main.js
@@ -23,21 +23,21 @@ app.use(cookieParser())
 
 app.get('/api/challengesCompleted', (req, res) => {
     if (req.cookies == undefined || req.cookies['auth'] == undefined) {
-        res.status = 401
+        res.status(401)
         res.end()
         return
     }
     checkUser(req.cookies['auth'])
-    res.status = 200
+    res.status(200)
     res.end(JSON.stringify(Array.from(userScoreMap.get(req.cookies['auth']))))
 })
 
 app.post('/api/check/:id', (req, res) => {
     if (req.params.id > challengeAnswers.length) {
-        res.status = 400
+        res.status(400)
         res.end()
     }
-    res.status = 200
+    res.status(200)
     if (req.body.answer === challengeAnswers[req.params.id]) {
         checkUser(req.cookies['auth'])
         userScoreMap.get(req.cookies['auth']).add(req.params.id)
